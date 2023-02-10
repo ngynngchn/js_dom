fc304b3adaa0a27ce811080170dd352dae2a7795
Hanni <ngynngchn@gmail.com>
2023-02-10 12:01:23 +0100

    Initial commit

---
+++ lev1_2/assets/js/main.js
@@ -0,0 +1,20 @@
+/* Aufgabenstellung
+
+
+Schreibe eine Funktion, die beim Anklicken der Buttons die Hintergrundfarben aller Elemente ändert.
+
+HTML- und CSS-Code findest du im Kommentarbereich.
+Nutze zum Beispiel
+getElementsByClassName
+for loop
+length
+Bonus: Wenn man nochmal den Button anklickt, ändert sich die Hintergrundfarbe wieder zurück.
+ */
+
+let elements = document.getElementsByClassName("example");
+
+function myFunction() {
+	Array.from(elements).forEach((element) => {
+		element.classList.toggle("bg");
+	});
+}
