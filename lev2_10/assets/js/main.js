1de626bfddf8f75ba5ebbf9e4c2554f5add8a161
Hanni <ngynngchn@gmail.com>
2023-02-10 14:12:03 +0100

    not done with lev2_10; will look at it later

---
+++ lev2_10/assets/js/main.js
@@ -0,0 +1,19 @@
+/* Aufgabenstellung
+
+
+Schreibe drei Funktionen, die beim Anklicken des Buttons den Style ändern.
+Der HTML und CSS Code befindet sich im Kommentar.
+Nutze
+classList
+transition-property
+transition-duration
+text-shadow
+
+ */
+
+let element = document.getElementById("element");
+const addClass = (elem) => {
+	console.log(element);
+	console.log(elem);
+	element.classList.add = elem;
+};
